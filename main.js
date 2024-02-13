11a9e34a0805c7df7c05d1eec3bb83cb43782e61
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-02-12 23:09:26 -0500

    Fix: Revise makeGrid function to calculate and set container width so number of rows in grid will be correct

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-for (let i = 0; i < 256; i++){
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('square');
-    document.getElementById('grid-container').appendChild(gridSquare);
+const input = Number(prompt('Choose a number between 1 and 100'));
+let totalSquares = Math.pow(input,2);
+const container = document.getElementById('grid-container');
+const containerWidth = container.offsetWidth;
+
+if (input > 0 && input < 100) {
+    makeGrid();
+} else alert('Please refresh the page and choose a number between 1 and 100');
+
+function makeGrid() {
+    const sideLength =  Math.round(containerWidth / input);
+    const sideLengthStr = sideLength + 'px';
+
+    for (let i = 0; i < totalSquares; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('square');
+        gridSquare.style.width = sideLengthStr;
+        gridSquare.style.height = sideLengthStr;
+        container.appendChild(gridSquare);
+        console.log(sideLengthStr);
+    }
+    const fullWidth = sideLength * input;
+    const fullWidthStr = fullWidth + 'px';
+    container.style.width = fullWidthStr;
 }
