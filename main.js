e016b8bc1718598311243d37a91c71cc38f982f4
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-02-12 20:59:13 -0500

    Feat: Add function to create 16 div elements w/in #grid-container

---
+++ main.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 16; i++){
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.add('square');
+    document.getElementById('grid-container').appendChild(gridSquare);
+}
